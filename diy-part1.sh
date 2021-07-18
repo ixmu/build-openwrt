ee80b53556306e53fe047269cb8eb6301ddc0b48
Xlous <35778015+ixmu@users.noreply.github.com>
2021-07-19 07:39:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,9 +16,9 @@
 # Add a feed source Plug-in
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#echo 'src-git smartdns https://github.com/pymumu/smartdns' >>feeds.conf.default
-#echo 'src-git luci-app-smartdns https://github.com/pymumu/luci-app-smartdns' >>feeds.conf.default
-#echo 'src-git luci-app-jd-dailybonus https://github.com/jerrykuku/luci-app-jd-dailybonus' >>feeds.conf.default
+git clone https://github.com/pymumu/smartdns package/smartdns
+git clone https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
 # Add a feed source Theme
-#echo 'src-git luci-theme-argon_armygree https://github.com/YL2209/luci-theme-argon_armygreen' >>feeds.conf.default
-#echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom' >>feeds.conf.default
+git clone https://github.com/YL2209/luci-theme-argon_armygreen package/luci-theme-argon_armygreen
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
