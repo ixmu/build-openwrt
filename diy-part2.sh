c4a1170e675e25007151fbe0a41387ae06bc0a66
Xlous <35778015+ixmu@users.noreply.github.com>
2021-07-21 07:52:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,4 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-#sed -i 's/5.10/5.4/g' target/linux/ramips/Makefile
+sed -i 's/5.10/5.4/g' target/linux/ramips/Makefile
